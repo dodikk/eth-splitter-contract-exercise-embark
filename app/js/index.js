a528a6fa3bf21069a9d477553c0fb07964764b42
adodatko <adodatko@dataart.com>
2018-10-18 14:09:57 +0300

    added app state update

--- app/js/index.js
+++ app/js/index.js
@@ -15,6 +15,9 @@ var _sender         = null;
 var _firstReceiver  = null;
 var _secondReceiver = null;
 
+var _splitterInstanceAddress = null;
+
+
 
 $(document).ready(
 async function ()
@@ -45,6 +48,51 @@ async function ()
 
 async function renderState()
 {
+    var senderBalance         = await web3.eth.getBalance(_sender                 );
+    var firstStrangerBalance  = await web3.eth.getBalance(_firstReceiver          );
+    var secondStrangerBalance = await web3.eth.getBalance(_secondReceiver         );
+    var contractBalance       = await web3.eth.getBalance(_splitterInstanceAddress);
+
+
+    var strSenderAddress = _sender;
+    var strSenderBalanceEth = web3.utils.fromWei(senderBalance);
+
+    $("#splitter_state_table #row_sender .col_address"    ).html(strSenderAddress   );
+    $("#splitter_state_table #row_sender .col_balance_wei").html(senderBalance      );
+    $("#splitter_state_table #row_sender .col_balance_eth").html(strSenderBalanceEth);
+
+
+    // =====
+    //
+    var strFirstReceiverAddress = _firstReceiver;
+    var strFirstReceiverBalanceEth = web3.utils.fromWei(firstStrangerBalance);
+
+    $("#splitter_state_table #row_recv_1 .col_address"    ).html(strFirstReceiverAddress   );
+    $("#splitter_state_table #row_recv_1 .col_balance_wei").html(firstStrangerBalance      );
+    $("#splitter_state_table #row_recv_1 .col_balance_eth").html(strFirstReceiverBalanceEth);
+
+
+    // =====
+    //
+    var strSecondReceiverAddress = _secondReceiver;
+    var strSecondReceiverBalanceEth = web3.utils.fromWei(secondStrangerBalance);
+
+    $("#splitter_state_table #row_recv_2 .col_address"    ).html(strSecondReceiverAddress   );
+    $("#splitter_state_table #row_recv_2 .col_balance_wei").html(secondStrangerBalance      );
+    $("#splitter_state_table #row_recv_2 .col_balance_eth").html(strSecondReceiverBalanceEth);
+
+
+
+    // =====
+    //
+    var strContractAddress = _splitterInstanceAddress;
+    var strContractBalanceEth = web3.utils.fromWei(secondStrangerBalance);
+
+    $("#splitter_state_table #row_contract .col_address"    ).html(strContractAddress   );
+    $("#splitter_state_table #row_contract .col_balance_wei").html(contractBalance      );
+    $("#splitter_state_table #row_contract .col_balance_eth").html(strContractBalanceEth);
+
+
 //    { this.state.web3.utils.fromWei( this.state.senderBalance, 'ether') }
 
 }
@@ -53,6 +101,12 @@ async function renderState()
 async function loadAccountsAsync()
 {
     _accounts = await web3.eth.getAccounts();
+
+    _splitterInstanceAddress = 
+        await AdkSplitterForTwo.methods
+                               .GetContractAddress()
+                               .call();
+
 
 
     // TODO: add more addresses in the config
