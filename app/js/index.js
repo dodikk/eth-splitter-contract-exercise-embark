37a2ad5efe51fec8794dfaf59172c1e13828f879
adodatko <adodatko@dataart.com>
2018-10-18 17:52:15 +0300

    [jquery] fixed table updates
    added logs for troubleshooting

--- app/js/index.js
+++ app/js/index.js
@@ -5,8 +5,7 @@ import EmbarkJS from 'Embark/EmbarkJS';
 //import SimpleStorage from 'Embark/contracts/SimpleStorage';
 
 import AdkSplitterForTwo from 'Embark/contracts/AdkSplitterForTwo';
-// import $ './jquery';
-var $ = require('jquery');
+const $ = require('jquery');
 
 
 var _accounts = null;
@@ -22,32 +21,59 @@ var _splitterInstanceAddress = null;
 $(document).ready(
 async function ()
 {
+     console.log("=== [BEGIN] document.ready");
+
      await loadAccountsAsync();
 
-     $("#splitter.button button").click(
+     $("#splitter_button button").click(
      async function ()
      {
+         console.log("=== [BEGIN] button clicked");
 
-         var splitTransactionReceipt = 
-         await AdkSplitterForTwo.methods
-         .Split(
-             _firstReceiver , 
-             _secondReceiver) 
-         .send(
+
+         try
          {
-             from    : _sender, 
-             value   :    2000, 
-             gasPrice:      '0' 
-         });
+             console.log("=== [BEGIN] split ether transaction");
 
+
+            var splitTransactionReceipt = 
+             await AdkSplitterForTwo.methods
+             .Split(
+                 _firstReceiver , 
+                 _secondReceiver) 
+             .send(
+             {
+                 from    : _sender, 
+                 value   :    2000, 
+                 gasPrice:      '0' 
+             });
+
+             console.log("=== [END] split ether transaction");
+         }
+         catch (ex)
+         {
+              console.log("Error splitting ether : ");
+              console.log(ex);
+         }
 
          renderState();
+
+
+         console.log("=== [END] button clicked");
      })
+
+    console.log("=== [END] document.ready");
+
 }) // func document.ready()
+
+
+
 
 
 async function renderState()
 {
+    console.log("=== [BEGIN] renderState()");
+
     var senderBalance         = await web3.eth.getBalance(_sender                 );
     var firstStrangerBalance  = await web3.eth.getBalance(_firstReceiver          );
     var secondStrangerBalance = await web3.eth.getBalance(_secondReceiver         );
@@ -56,6 +82,7 @@ async function renderState()
 
     var strSenderAddress = _sender;
     var strSenderBalanceEth = web3.utils.fromWei(senderBalance);
+
 
     $("#row_sender .col_address"    ).html(strSenderAddress   );
     $("#row_sender .col_balance_wei").html(senderBalance      );
@@ -67,9 +94,9 @@ async function renderState()
     var strFirstReceiverAddress = _firstReceiver;
     var strFirstReceiverBalanceEth = web3.utils.fromWei(firstStrangerBalance);
 
-    $("#row_recv_1 .col_address"    ).html(strFirstReceiverAddress   );
-    $("#row_recv_1 .col_balance_wei").html(firstStrangerBalance      );
-    $("#row_recv_1 .col_balance_eth").html(strFirstReceiverBalanceEth);
+    $("#row_recv_1 .col_address"    ).text(strFirstReceiverAddress   );
+    $("#row_recv_1 .col_balance_wei").text(firstStrangerBalance      );
+    $("#row_recv_1 .col_balance_eth").text(strFirstReceiverBalanceEth);
 
 
     // =====
@@ -77,33 +104,64 @@ async function renderState()
     var strSecondReceiverAddress = _secondReceiver;
     var strSecondReceiverBalanceEth = web3.utils.fromWei(secondStrangerBalance);
 
-    $("#row_recv_2 .col_address"    ).html(strSecondReceiverAddress   );
-    $("#row_recv_2 .col_balance_wei").html(secondStrangerBalance      );
-    $("#row_recv_2 .col_balance_eth").html(strSecondReceiverBalanceEth);
+    $("#row_recv_2 .col_address"    ).text(strSecondReceiverAddress   );
+    $("#row_recv_2 .col_balance_wei").text(secondStrangerBalance      );
+    $("#row_recv_2 .col_balance_eth").text(strSecondReceiverBalanceEth);
 
 
 
     // =====
     //
     var strContractAddress = _splitterInstanceAddress;
-    var strContractBalanceEth = web3.utils.fromWei(secondStrangerBalance);
+    var strContractBalanceEth = web3.utils.fromWei(contractBalance);
 
-    $("#row_contract .col_address"    ).html(strContractAddress   );
-    $("#row_contract .col_balance_wei").html(contractBalance      );
-    $("#row_contract .col_balance_eth").html(strContractBalanceEth);
+    $("#row_contract .col_address"    ).text(strContractAddress   );
+    $("#row_contract .col_balance_wei").text(contractBalance      );
+    $("#row_contract .col_balance_eth").text(strContractBalanceEth);
 
 
+    console.log(`|Sender         | ${strSenderAddress} | ${senderBalance} wei | ${strSenderBalanceEth} ether |` );
+    console.log(`|First receiver | ${strFirstReceiverAddress} | ${firstStrangerBalance} wei | ${strFirstReceiverBalanceEth} ether |` );
+    console.log(`|Second receiver| ${strSecondReceiverAddress} | ${secondStrangerBalance} wei | ${strSecondReceiverBalanceEth} ether |` );
+    console.log(`|Contract       | ${strContractAddress} | ${contractBalance} wei | ${strContractBalanceEth} ether |` );
+
+
+    console.log("=== [END] renderState()");
 }
 
 
 async function loadAccountsAsync()
 {
-    _accounts = await web3.eth.getAccounts();
+    console.log("=== [BEGIN] loadAccountsAsync");
 
+    var strGlobalContract = JSON.stringify(AdkSplitterForTwo);
+    var globalContractType = typeof AdkSplitterForTwo;
+
+    console.log(`Global contract : ${strGlobalContract}`);
+    console.log(`Global contract type: ${globalContractType}`);
+
+    console.log("=== [BEGIN] eth.getAccounts()");
+    _accounts = await web3.eth.getAccounts();
+    console.log("=== [END] eth.getAccounts()");
+
+
+    var accountsType = typeof _accounts;
+    console.log(`accounts : ${_accounts}`);
+    console.log(`accounts type : ${accountsType}`);
+
+
+    console.log("=== [BEGIN] get contract address");
     _splitterInstanceAddress = 
         await AdkSplitterForTwo.methods
                                .GetContractAddress()
-                               .call();
+                               .call(
+                                   {
+                                       from    : _accounts[0],
+                                       gasPrice: '0'
+                                   });
+    console.log("=== [END] get contract address");
+
+
 
 
 
@@ -114,6 +172,14 @@ async function loadAccountsAsync()
     _secondReceiver = _accounts[2];
 
 
+     console.log(`_sender                  : ${_sender}`                 );
+     console.log(`_firstReceiver           : ${_firstReceiver}`          );
+     console.log(`_secondReceiver          : ${_secondReceiver}`         );
+     console.log(`_splitterInstanceAddress : ${_splitterInstanceAddress}`);
+
+
     await renderState();
+
+    console.log("=== [END] loadAccountsAsync");
 }
 
